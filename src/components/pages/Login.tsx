a37d02bec2d75ec2aaf6600e674de8bac7be2160
Yasu1025 <sillyjp25@gmail.com>
2022-01-04 02:57:55 +0900

    Login Template

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,18 @@
+import { Box, Divider, Flex, Heading, Input, Stack } from '@chakra-ui/react'
 import { memo, VFC } from 'react'
+import { PrimaryButton } from '../atoms/button/PrimaryButton'
 
 export const Login: VFC = memo(() => (
-  <div>
-    <p>login</p>
-  </div>
+  <Flex align='center' justify='center' height='100vh'>
+    <Box bg='white' w='sm' p={4} borderRadius='md' shadow='md'>
+      <Heading as='h1' size='lg' textAlign='center'>
+        User Manage App
+      </Heading>
+      <Divider my={4} />
+      <Stack spacing={6} px={10}>
+        <Input placeholder='User ID' />
+        <PrimaryButton>Login</PrimaryButton>
+      </Stack>
+    </Box>
+  </Flex>
 ))
